f1b231326f575f8551ded00f1b34b6ce217e7213
Wurdsmith <wurdsmithing@gmail.com>
2021-01-31 01:07:38 -0600

    commit tree b428c8262da65dcedfee9327d1a7270f144187ab

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    def initialize (url)
+        @url = url
+    end
+
+    def get_response_body 
+        request = URI.parse(@url)
+        response = Net::HTTP.get_response(request)
+        response.body
+    end
+
+    def parse_json
+        hash = JSON.parse(self.get_response_body)
+    end
+end
